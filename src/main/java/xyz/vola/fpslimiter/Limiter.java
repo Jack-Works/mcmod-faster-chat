d86a1a60e3e5b4677729191ec96bfa80b685d79e
Jack Works <zjwpeter@gmail.com>
2018-04-05 16:12:11 +0800

    Fix that sometimes fps will be lock at 5

--- src/main/java/xyz/vola/fpslimiter/Limiter.java
+++ src/main/java/xyz/vola/fpslimiter/Limiter.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 public class Limiter {
     public static final String MODID = "fpslimiter";
     public static final String NAME = "Faster Chat";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     private static final Minecraft mc = Minecraft.getMinecraft();
 
     private static Logger logger;
@@ -47,6 +47,9 @@ public class Limiter {
     @SubscribeEvent
     public static void onRestoreFPS(GuiOpenEvent event) {
         if (event.getGui() == null) {
+            if (lastFps < 30) {
+                lastFps = 60;
+            }
             ChangeFPS(lastFps);
         }
     }
